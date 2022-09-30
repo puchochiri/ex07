33bf0fc93b16f1f3730251f5783039cb1c7ffca7
양선우 <@DESKTOP-94O1RKP>
2022-09-30 16:18:35 +0900

    20220930 commit

--- ex07/02.sql/ex04.sql
+++ ex07/02.sql/ex04.sql
@@ -94,3 +94,39 @@ ALTER TABLE tbl_reply DROP CONSTRAINT FK_REPLY_BOARD;
 -- 제약조건 수정
 ALTER TABLE tbl_reply ADD CONSTRAINT FK_REPLY_BOARD FOREIGN KEY(bno) 
 REFERENCES tbl_board (bno) ON DELETE CASCADE;
+
+
+
+-- 스프링 시큐리티의 지정된 테이블을 생성하는 SQL
+
+create table users(
+    username varchar2(50) not null primary key,
+    password varchar2(50) not null,
+    enabled char(1) default '1'
+);
+
+
+create table authorities (
+    username varchar2(50) not null,
+    authority varchar2(50) not null,
+    constraint fk_authorities_users foreign key(username) references users(username)
+    
+);
+
+create unique index ix_auth_username on authorities (username, authority);
+
+insert into users (username, password) values ('user00', 'pw00');
+insert into users (username, password) values ('member00', 'pw00');
+insert into users (username, password) values ('admin00', 'pw00');
+
+insert into authorities (username, authority) values ('user00', 'ROLE_USER');
+insert into authorities (username, authority) values ('member00', 'ROLE_MANAGER');
+insert into authorities (username, authority) values ('admin00', 'ROLE_MANAGER');
+insert into authorities (username, authority) values ('admin00', 'ROLE_ADMIN');
+    
+commit;
+
+
+select * from users
+
+select * from authorities
