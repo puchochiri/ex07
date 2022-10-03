a70f08742652571d6136ec4fb1d140b73f636590
puchochiri <doawishfor@naver.com>
2022-10-03 07:09:23 +0530

    20221003 commit

--- ex07/02.sql/ex04.sql
+++ ex07/02.sql/ex04.sql
@@ -130,3 +130,19 @@ commit;
 select * from users
 
 select * from authorities
+
+create table tbl_member(
+    userid varchar2(50) not null primary key,
+    userpw varchar2(100) not null,
+    username varchar2(100) not null,
+    regdate date default sysdate,
+    updatedate date default sysdate,
+    enabled char(1) default '1');
+)
+
+
+create table tbl_member_auth (
+    userid varchar2(50) not null,
+    auth    varchar2(50) not null,
+    constraint fk_member_auth foreign key(userid) references tbl_member(userid)
+);
